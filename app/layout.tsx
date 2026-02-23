6e6baf83ad20849d15372063bad098639ad9123d
PrimeTimeTran <primetimetran@gmail.com>
2026-02-23 14:52:21 +0700

    Add GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import Script from 'next/script'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 
@@ -13,6 +14,20 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-R8G7LNXFC9"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R8G7LNXFC9', {
+                send_page_view: false,
+            });
+          `}
+        </Script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
